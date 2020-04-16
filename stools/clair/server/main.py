1594f55d728ac84774a8dbb257bb742f66333cfa
Vlad Doster <mvdoster@gmail.com>
2020-04-16 18:28:20 -0500

    revert changes

--- stools/clair/server/main.py
+++ stools/clair/server/main.py
@@ -1,20 +1,15 @@
 '''
-
 Copyright (C) 2018-2019 Vanessa Sochat.
-
 This program is free software: you can redistribute it and/or modify it
 under the terms of the GNU Affero General Public License as published by
 the Free Software Foundation, either version 3 of the License, or (at your
 option) any later version.
-
 This program is distributed in the hope that it will be useful, but WITHOUT
 ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
 FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public
 License for more details.
-
 You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
 '''
 
 from aiohttp import web
@@ -25,7 +20,7 @@ from stools.clair.server.routes import (
 )
 
 
-def start(port=8080, host='127.0.0.1', static_folder='/var/www/images'):
+def start(port=8080, host='0.0.0.0', static_folder='/var/www/images'):
 
     app = web.Application()
     setup_routes(app)
